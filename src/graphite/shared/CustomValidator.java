41630ce5a11e5c2a09ff601e9478e65a518f023d
ionutpredoaia <ionutpredoaia@gmail.com>
2023-03-15 20:58:18 +0000

    Fix EObject Validator bug
    - removed Graphite's EditorStartup class and added a M2T transformation that generates EditorStartup classes for each package
    - changed the identifier of model elements in the Problems View to the URI of a model element

--- src/graphite/shared/CustomValidator.java
+++ src/graphite/shared/CustomValidator.java
@@ -4,9 +4,11 @@ import java.util.Map;
 import org.eclipse.emf.common.util.BasicDiagnostic;
 import org.eclipse.emf.common.util.Diagnostic;
 import org.eclipse.emf.common.util.DiagnosticChain;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.util.EObjectValidator;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.xtext.diagnostics.AbstractDiagnostic;
 
 import graphite.textual.XtextEObject;
@@ -20,16 +22,11 @@ public class CustomValidator extends EObjectValidator {
 			for (DerivedObjectProperties derivedObjectProperties: xtextEObject.getDerivedObjectsProperties().values()) {
 				if (derivedObjectProperties.isInvalid()) {
 					isValid = false;			
-					try {
-						String objectIdentifier = (String) derivedObjectProperties.getIdentifierGetterMethod().invoke(eObject);
-						AbstractDiagnostic xtextDiagnostic = (AbstractDiagnostic) derivedObjectProperties.getParseErrors().get(0);
-						String erroMessage = "Property '" + derivedObjectProperties.getParsedStringName() + "' of " + objectIdentifier + " could not be parsed: " + xtextDiagnostic.getMessage();
-						BasicDiagnostic basicDiagnostic = new BasicDiagnostic(Diagnostic.ERROR, DIAGNOSTIC_SOURCE,  0, erroMessage, new Object[] {eObject, xtextDiagnostic.getLocation()});
-						diagnostics.add(basicDiagnostic);
-					}
-					catch (Exception e) {
-						e.printStackTrace();
-					}
+					URI uri = EcoreUtil.getURI(eObject);
+					AbstractDiagnostic xtextDiagnostic = (AbstractDiagnostic) derivedObjectProperties.getParseErrors().get(0);
+					String erroMessage = "Property '" + derivedObjectProperties.getParsedStringName() + "' of " + uri.toString() + " could not be parsed: " + xtextDiagnostic.getMessage();
+					BasicDiagnostic basicDiagnostic = new BasicDiagnostic(Diagnostic.ERROR, DIAGNOSTIC_SOURCE,  0, erroMessage, new Object[] {eObject, xtextDiagnostic.getLocation()});
+					diagnostics.add(basicDiagnostic);
 				}
 			}
 			return isValid;
